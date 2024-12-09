b639d6c1bd638efe91325203b439e843a5a01181
Abdo0Gego0 <abdo.gego.2000@gmail.com>
2024-12-09 02:34:02 -0800

    Build: Function to input the player choice Using the prompt to input the choice then change to lower case so it can be case insinsative then check it if it's rock or paper or scissors

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -7,5 +7,21 @@ function getComputerChoice(){
     else
         return "Scissors"
 }
+function getHumanChoice(){
+    let x = prompt("Enter Rock or Paper or Scissors","Rock");
+    x= x.toLowerCase();
+    switch(x){
+        case "rock":
+            return "Rock";
+            break;
+        case "paper":
+            return "Paper";
+        case "scissors":
+            return "Scissors";
+        default:
+            return 0;
+
+    }
+}
 
 console.log(getComputerChoice());
